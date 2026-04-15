4bca678bf0ac04c661f7a10961ca271c90cd873e
guibenatte <guibenattedev@gmail.com>
2026-04-15 20:10:48 -0300

    Feat: Criando classe

--- Scool/src/Professor.java
+++ Scool/src/Professor.java
@@ -1,2 +1,22 @@
-public class Professor {
+public class Professor extends Pessoa{
+    private String disciplina;
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public Professor(String nome, String disciplina){
+        setNome(nome);
+        this.disciplina = disciplina;
+    }
+
+    @Override
+
+    public void apresentar(){
+        System.out.printf("Sou o professor %s, leciono %s.", getNome(), getDisciplina());
+    }
 }
