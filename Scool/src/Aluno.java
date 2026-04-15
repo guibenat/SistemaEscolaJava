d54db089f20b3149dc3d1637def8502eec8f85e9
guibenatte <guibenattedev@gmail.com>
2026-04-15 19:50:52 -0300

    Feat: Criando sub-classe Aluno.

--- Scool/src/Aluno.java
+++ Scool/src/Aluno.java
@@ -1,4 +1,23 @@
 public class Aluno extends Pessoa{
+    private String matricula;
 
+    public Aluno(String nome, int idade, String Matricula) {
+        setNome(nome);
+        setIdade(idade);
+        this.matricula = matricula;
+    }
+
+    public String getMatricula(){
+        return matricula;
+    }
+    public void setMatricula(String matricula){
+        this.matricula = matricula;
+    }
+
+    @Override
+
+    public void apresentar() {
+        System.out.printf("Sou o aluno %s, tenho %d anos, matrícula %s.", getNome(), getIdade(), getMatricula());
+    }
 
 }
